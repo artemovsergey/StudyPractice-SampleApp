221138a4957580ad46cfe2cc1f0f7ffa05d79939
sergey <artik3314@gmail.com>
2025-11-28 21:18:31 +0300

    Настройка аннотаций Swagger

--- SampleApp.API/Controllers/UsersController.cs
+++ SampleApp.API/Controllers/UsersController.cs
@@ -1,8 +1,8 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SampleApp.API.Entities;
 using SampleApp.API.Interfaces;
 using SampleApp.API.Validations;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace SampleApp.API.Controllers;
 
@@ -17,6 +17,12 @@ public class UsersController : ControllerBase
         _repo = repo;
     }
 
+    [SwaggerOperation(
+        Summary = "Создание пользователя",
+        Description = "Возвращает нового пользователя",
+        OperationId = "CreateUser"
+    )]
+    [SwaggerResponse(200, "Пользователь создан успешно", typeof(User))]
     [HttpPost]
     public ActionResult CreateUser(User user)
     {
@@ -30,24 +36,52 @@ public class UsersController : ControllerBase
         return Ok(_repo.CreateUser(user));
     }
 
+    [SwaggerOperation(
+        Summary = "Получение списка пользователей",
+        Description = "Возвращает все пользователей",
+        OperationId = "GetProducts"
+    )]
+    [SwaggerResponse(200, "Список пользователей получен успешно", typeof(List<User>))]
+    [SwaggerResponse(404, "Пользователи не найдены")]
     [HttpGet]
     public ActionResult GetUsers()
     {
         return Ok(_repo.GetUsers());
     }
 
+    [SwaggerOperation(
+        Summary = "Обновление пользователя",
+        Description = "Возвращает отредактированного пользователя",
+        OperationId = "UpdateUser"
+    )]
+    [SwaggerResponse(200, "Пользователь обновлен успешно", typeof(User))]
+    [SwaggerResponse(404, "Пользователь не найден")]
     [HttpPut]
     public ActionResult UpdateUser(User user)
     {
         return Ok(_repo.EditUser(user, user.Id));
     }
 
+    [SwaggerOperation(
+        Summary = "Поиск пользователя по id",
+        Description = "Возвращает найденного пользователя или исключение",
+        OperationId = "GetUserById"
+    )]
+    [SwaggerResponse(200, "Пользователь найден успешно", typeof(User))]
+    [SwaggerResponse(404, "Пользователь не найден")]
     [HttpGet("{id}")]
     public ActionResult GetUserById(int id)
     {
         return Ok(_repo.FindUserById(id));
     }
 
+    [SwaggerOperation(
+        Summary = "Удаление пользователя по id",
+        Description = "Возвращает true или исключение",
+        OperationId = "DeleteUser"
+    )]
+    [SwaggerResponse(200, "Пользователь удален успешно", typeof(User))]
+    [SwaggerResponse(404, "Пользователь не найден")]
     [HttpDelete("{id}")]
     public ActionResult DeleteUser(int id)
     {
--- SampleApp.API/Program.cs
+++ SampleApp.API/Program.cs
@@ -1,9 +1,27 @@
+using Microsoft.OpenApi;
 using SampleApp.API.Interfaces;
 using SampleApp.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+    c.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Title = "SampleApp",
+            Version = "v1",
+            Description = "API для пользователей",
+            Contact = new OpenApiContact
+            {
+                Url = new Uri("http://prep.scc/~asv"),
+                Email = "asv@prep.scc",
+            },
+        }
+    );
+});
 builder.Services.AddSingleton<IUserRepository, UsersMemoryRepository>();
 
 var app = builder.Build();
